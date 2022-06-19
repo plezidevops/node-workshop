d50a5e9db8063c13ee304df984c66c13c9268360
pascalmlaurent@gmail.com <plaurent@pascal.jeremieonline.com>
2022-06-19 09:44:32 -0400

    intro to node js

---
+++ intro-node/index.js
@@ -0,0 +1,8 @@
+const superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+const mySuperHeroeName = superheroes.random();
+const mySupervillainName = supervillains.random();
+
+console.log(mySuperHeroeName);
+console.log(mySupervillainName);

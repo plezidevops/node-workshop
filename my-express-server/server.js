b64ef3fa1a17342b7b7016a693fed8bd050bddab
Plezi Devops <plezidevops@gmail.com>
2022-06-19 22:15:07 -0400

    creating routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,9 +1,24 @@
+const { application } = require('express');
 const express = require('express');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact me at: pascal@gmail.com');
+});
+
+app.get('/about', (req, res) => {
+  res.send(`
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Expedita aliquid impedit nam voluptatem facilis officia porro repudiandae neque officiis earum totam vitae, provident aperiam optio maxime? Inventore obcaecati nihil quam.
+  `);
+});
+
+app.get('/hobbies', (req, res) => {
+  res.send('<h1>Model Airplanes</h1>');
 });
 
 app.listen(port, () => {
